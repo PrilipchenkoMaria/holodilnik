bc0e28ce0db912ed6de16d21540528f2ee374bb8
PrilipchenkoMaria <largo.mari@yandex.ua>
2019-08-29 16:17:55 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="Wrapper">
+                <header className="AppHeader">
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href="" className="SignUpButton">Регистрация</a>
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href="" className="SignInButton">Вход</a>
+                </header>
+                <aside className="MineIngredients">
+
+                </aside>
+                <div className="Content">
+                    <div className="SearchIngredient">
+                        <form>
+                            <input type="search" name="q" placeholder="Найти ингридиент"/>
+                            <input type="submit" value="Найти"/>
+                        </form>
+                    </div>
+                    <div className="IngredientsList">
+
+                    </div>
+                    <div className="RecipesButtons">
+
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a href="" className="ShowAllRecipesButton">Найти рецепты</a>
+
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a href="" className="RandomRecipeButton">Случайный рецепт</a>
+
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a href="" className="CreateRecipeButton">Добавить рецепт</a>
+
+                    </div>
+                    <div className="SearchRecipesResults">
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
