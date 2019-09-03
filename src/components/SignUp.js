fee64eb7dcdd1761b8d8e3e49adca600c7058e92
PrilipchenkoMaria <largo.mari@yandex.ua>
2019-09-03 17:02:11 +0300

    SignUp page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,24 @@
 import React from 'react';
+
 export function SignUp() {
     return (
-        <div>
+        <div className="SignUpPage">
             <h1>Регистрация</h1>
-
+            <form className="SignUpForm">
+                <label>
+                    <input type="text" placeholder="Имя пользователя"/>
+                </label>
+                <label>
+                    <input type="email" placeholder="email"/>
+                </label>
+                <label>
+                    <input type="password" placeholder="Пароль"/>
+                </label>
+                <label>
+                    <input type="password" placeholder="Повтор пароля"/>
+                </label>
+                <input className="SignUpSubmit" type="submit" value="Зарегистрироваться"/>
+            </form>
         </div>
 
     )
