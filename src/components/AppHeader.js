e0152c0db44fcb6faf508376d9482de8458cf445
PrilipchenkoMaria <largo.mari@yandex.ua>
2019-09-05 01:58:21 +0300

    Home link

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
+import Logo from '../img/AppHeaderBg.png';
 
 
-export function AppHeader() {
-    return (
-        <>
-            <Link to="/SignUp" className="SignUpButton">Регистрация</Link>
-            <Link to="/SignIn" className="SignInButton">Войти</Link>
-            <div className="SignInModal">
-                <h1>Вход</h1>
-                <form className="SignInForm">
-                    <label>
-                        <input type="email" placeholder="email"/>
-                    </label>
-                    <label>
-                        <input type="password" placeholder="Пароль"/>
-                    </label>
-                    <input className="SignInSubmit" type="submit" value="Войти"/>
-                </form>
-            </div>
-        </>
+function SignInForm() {
+    return <div className="SignInModal">
+        <h1>Вход</h1>
+        <form className="SignInForm">
+            <label>
+                <input type="email" placeholder="email"/>
+            </label>
+            <label>
+                <input type="password" placeholder="Пароль"/>
+            </label>
+            <input className="SignInSubmit" type="submit" value="Войти"/>
+        </form>
+    </div>;
+}
 
-    );
+export class AppHeader extends React.Component {
+
+    state = {
+        isSignInShown: false,
+    };
+
+    toggleSignIn() {
+        this.setState({
+            isSignInShown: !this.state.isSignInShown,
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <Link to="/"><img src={Logo} alt="logo"/></Link>
+                <Link to="/SignUp" className="SignUpButton">Регистрация</Link>
+                <a href="#" className="SignInButton" onClick={() => this.toggleSignIn()}>Войти</a>
+                {this.state.isSignInShown && <SignInForm/>}
+            </>
+        );
+    }
 }
 
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Logo from "../img/AppHeaderBg.png";
+import {Link} from "react-router-dom";
 
 export function SignUp() {
     return (
         <div className="SignUpPage">
+            <Link to="/"><img src={Logo} alt="logo"/></Link>
             <h1>Регистрация</h1>
             <form className="SignUpForm">
                 <label>
@@ -17,7 +20,7 @@ export function SignUp() {
                 <label>
                     <input type="password" placeholder="Повтор пароля"/>
                 </label>
-                <input className="SignUpSubmit" type="submit" value="Зарегистрироваться"/>
+                <Link to="/"><input className="SignUpSubmit" type="submit" value="Зарегистрироваться"/></Link>
             </form>
         </div>
 
