0f7f887b75339b1883ff7b3dddb9cd686f701e5a
PrilipchenkoMaria <largo.mari@yandex.ua>
2019-09-09 18:10:17 +0300

    handleInputChange

--- src/components/Content/CreateRecipe.js
+++ src/components/Content/CreateRecipe.js
@@ -1,40 +1,78 @@
 import React from 'react';
 
-export function CreateRecipe() {
-    return (
-        <div className="CreateRecipe">
-            <h1>Добавление рецепта</h1>
-            <form className="CreateRecipeForm">
-                <label>
-                    Название блюда:
-                    <input className="InputTextRecipeForm" type="text"/>
-                </label>
-                <label>
-                    Краткое описание:
-                    <textarea className="InputTextareaRecipeForm"/>
-                </label>
-                <label>
-                    Время приготовления:
-                    <input className="InputNumberRecipeForm" type="number"/>
-                    (минут)
-                </label>
-                <label>
-                    Количество порций:
-                    <input className="InputNumberRecipeForm" type="number"/>
-                </label>
-                <label>
-                    Ингридиенты:
-                    <input type="search" className="InputTextRecipeForm" placeholder="Найти ингридиент"/>
-                    <input className="InputNumberRecipeForm" type="number"/> гр.
-                    <input className="AddRecipeIngredient" type="submit" value="+"/>
-                </label>
-                <label>
-                    Процесс приготовления:
-                    <textarea className="InputTextareaRecipeForm"/>
-                </label>
-                <input className="CreateRecipeSubmit" type="submit" value="Добавить рецепт"/>
-            </form>
-        </div>
+export class CreateRecipe extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            dishName: '',
+            shortDescription: '',
+            cookingTime: '',
+            portionsNumber: '',
+            weight:'',
+            searchIngredient: '',
+            description: ''
+        }
 
-    )
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleAddRecipeIngredient = this.handleAddRecipeIngredient.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value =  target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    handleAddRecipeIngredient (event) {
+
+        event.preventDefault();
+    }
+    handleSubmit (event) {
+
+        event.preventDefault();
+    }
+
+
+    render() {
+        return (
+            <div className="CreateRecipe">
+                <h1>Добавление рецепта</h1>
+                <form className="CreateRecipeForm">
+                    <label>
+                        Название блюда:
+                        <input className="InputTextRecipeForm" type="text" name="dishName" value={this.state.dishName} onChange={this.handleInputChange}/>
+                    </label>
+                    <label>
+                        Краткое описание:
+                        <textarea className="InputTextareaRecipeForm" name="shortDescription" value={this.state.shortDescription} onChange={this.handleInputChange}/>
+                    </label>
+                    <label>
+                        Время приготовления:
+                        <input className="InputNumberRecipeForm" type="number" name="cookingTime" value={this.state.cookingTime} onChange={this.handleInputChange}/>
+                        (минут)
+                    </label>
+                    <label>
+                        Количество порций:
+                        <input className="InputNumberRecipeForm" type="number"  name="portionsNumber" value={this.state.portionsNumber} onChange={this.handleInputChange}/>
+                    </label>
+                    <label>
+                        Ингридиенты:
+                        <input type="search" className="InputTextRecipeForm" placeholder="Найти ингридиент" name="searchIngredient" value={this.state.searchIngredient} onChange={this.handleInputChange}/>
+                        <input className="InputNumberRecipeForm" type="number" name="weight" value={this.state.weight} onChange={this.handleInputChange}/> гр.
+                        <input className="AddRecipeIngredient" type="submit" value="+"  onClick={this.handleAddRecipeIngredient}/>
+                    </label>
+                    <label>
+                        Процесс приготовления:
+                        <textarea className="InputTextareaRecipeForm" name="description" value={this.state.description} onChange={this.handleInputChange}/>
+                    </label>
+                    <input className="CreateRecipeSubmit" type="submit" value="Добавить рецепт" onClick={this.handleSubmit}/>
+                </form>
+            </div>
+
+        )
+    }
 }
