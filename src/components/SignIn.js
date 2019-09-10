ec9aef906c2f6e7a7be3931b21accfe174f8d441
PrilipchenkoMaria <largo.mari@yandex.ua>
2019-09-10 21:16:48 +0300

    handleInputChange SignIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,25 +3,55 @@ import {Link} from 'react-router-dom';
 import Logo from '../img/AppHeaderBg.png';
 
 
-export function SignIn () {
-    
-    return <div className="SignInPage">
-        <Link to="/"><img src={Logo} alt="logo"/></Link>
-        <h1>Вход</h1>
-        <form className="SignInForm">
-            <label>
-                <input className="SignInFormInput" type="email" placeholder="email"/>
-            </label>
-            <label>
-                <input className="SignInFormInput" type="password" placeholder="Пароль"/>
-            </label>
-            <Link to="/"><input className="SignInSubmit" type="submit" value="Войти"/></Link>
-        </form>
-        <p>
-            <Link to="/ForgotPassword" className="ForgotPasswordLink">
-                Не помню пароль...
-            </Link>
-        </p>
+export class SignIn extends React.Component {
+    state = {
+        email: '',
+        password: '',
+    };
 
-    </div>
+    password = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({[name]: value});
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    render() {
+        return <div className="SignInPage">
+            <Link to="/"><img src={Logo} alt="logo"/></Link>
+            <h1>Вход</h1>
+            <form className="SignInForm" onSubmit={this.handleSubmit}>
+                <label>
+                    <input className="SignInFormInput"
+                           type="email"
+                           placeholder="email"
+                           name="email"
+                           value={this.state.email}
+                           onChange={this.handleInputChange}
+                    />
+                </label>
+                <label>
+                    <input className="SignInFormInput"
+                           type="password"
+                           placeholder="Пароль"
+                           name="password"
+                           value={this.state.password}
+                           onChange={this.handleInputChange}
+                    />
+                </label>
+                <Link to="/"><input className="SignInSubmit" type="submit" value="Войти"/></Link>
+            </form>
+            <p>
+                <Link to="/ForgotPassword" className="ForgotPasswordLink">
+                    Не помню пароль...
+                </Link>
+            </p>
+
+        </div>
+    }
 }
