1004c43fa2f1331d2acf613370300484957c7482
PrilipchenkoMaria <largo.mari@yandex.ua>
2020-03-18 20:58:08 +0300

    add insertSet to save ingredients and spices to the db

--- grabber/src/strategies/ChooseIngredientsFromRecipes.js
+++ grabber/src/strategies/ChooseIngredientsFromRecipes.js
@@ -1,10 +1,16 @@
 const MongoClient = require("mongodb").MongoClient;
-const {dbGrabConfig} = require("../../../config");
+const {dbGrabConfig, testUsersDBConfig} = require("../../../config");
 
 
 main().catch(console.error);
 
 async function main() {
+    const ingredientsAndSpices = await separateIngredientsAndSpices();
+    await insertSet(ingredientsAndSpices.ingredients, "ingredients");
+    await insertSet(ingredientsAndSpices.spices, "spices");
+}
+
+async function separateIngredientsAndSpices() {
     const client = await new MongoClient.connect(dbGrabConfig.mongo.url, {useUnifiedTopology: true});
     const collection = client.db(dbGrabConfig.mongo.dbname).collection("eda.ru");
     const recipesIngredients = await collection.find({}, {projection: {_id: 0, recipeIngredient: 1}}).toArray();
@@ -22,12 +28,20 @@ async function main() {
             let parenthesis = new RegExp(/\(/);
             let byTastePreposition = new RegExp(/\sпо\s/);
 
-            if (byTastePreposition.exec(item)) spices.add(item);
+            if (byTastePreposition.exec(item)) spices.add(item.split(byTastePreposition, 1).join());
             if (num.exec(item)) item = item.split(num, 1).join();
             if (preposition.exec(item)) item = item.split(preposition, 1).join();
             if (parenthesis.exec(item)) item = item.split(parenthesis, 1).join();
             names.add(item);
         });
     });
+    await client.close();
+    return ({ingredients: names, spices: spices});
+}
+
+async function insertSet(set, collectionName) {
+    const client = await new MongoClient.connect(testUsersDBConfig.mongo.url, {useUnifiedTopology: true});
+    const collection = client.db(testUsersDBConfig.mongo.dbname).collection(collectionName);
+    await collection.insertOne({ingredients:Array.from(set)});
     await client.close();
 }
